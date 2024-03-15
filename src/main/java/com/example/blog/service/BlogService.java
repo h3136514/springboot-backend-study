6e971929df816d1148ce9010205d00ef637e1227
h3136514 <3136514@naver.com>
2024-03-15 19:43:58 +0900

    feat: BlogService 생성

---
+++ src/main/java/com/example/blog/service/BlogService.java
@@ -0,0 +1,18 @@
+package com.example.blog.service;
+
+import com.example.blog.domain.Article;
+import com.example.blog.dto.AddArticleRequest;
+import com.example.blog.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
