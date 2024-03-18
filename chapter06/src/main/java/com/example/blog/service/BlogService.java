01285c9b6a0841df265e00e54338010d32f636da
h3136514 <3136514@naver.com>
2024-03-18 20:35:26 +0900

    rename: chapter07 합치기

---
+++ chapter06/src/main/java/com/example/blog/service/BlogService.java
@@ -0,0 +1,45 @@
+package com.example.blog.service;
+
+import com.example.blog.domain.Article;
+import com.example.blog.dto.AddArticleRequest;
+import com.example.blog.dto.UpdateArticleRequest;
+import com.example.blog.repository.BlogRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+    }
+
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
+}
