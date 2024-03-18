05617cd7916a9fc59c5dda569ffeb1d3994396cb
h3136514 <3136514@naver.com>
2024-03-18 18:45:26 +0900

    feat: 블로그 글 뷰 구현

--- src/main/java/com/example/blog/controller/BlogViewController.java
+++ src/main/java/com/example/blog/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.example.blog.controller;
 
+import com.example.blog.domain.Article;
 import com.example.blog.dto.ArticleListViewResponse;
+import com.example.blog.dto.ArticleViewResponse;
 import org.springframework.ui.Model;
 import com.example.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 @RequiredArgsConstructor
@@ -25,4 +28,11 @@ public class BlogViewController {
         return "articleList";
     }
 
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
 }
